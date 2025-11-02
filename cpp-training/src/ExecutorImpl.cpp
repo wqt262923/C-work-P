bcb7ef6df7dc1a56b45a0cc6c1d442f9428db663
wqt262923 <uxlnw58cdu64@163.com>
2025-11-02 15:41:37 +0800

    应用表驱动消减圈复杂度，功能代码实现

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -1,5 +1,6 @@
 #include "ExecutorImpl.hpp"
 #include <memory>
+#include <unordered_map>
 #include "Command.hpp"
 namespace adas
 {
@@ -12,31 +13,19 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
+    std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+    cmderMap.emplace('M', std::make_unique<MoveCommand>());
+    cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+    cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+    cmderMap.emplace('F', std::make_unique<FastCommand>());
     for (const auto cmd : commands) 
     {
-        std::unique_ptr<ICommand> cmder;
-        if (cmd == 'M') {
-            //Move();
-            cmder = std::make_unique<MoveCommand>();
-        } 
-        else if (cmd == 'L') {
-            //TurnLeft();
-            cmder = std::make_unique<TurnLeftCommand>();
-        } 
-        else if (cmd == 'R') {
-            //TurnRight();
-            cmder = std::make_unique<TurnRightCommand>();
-        }
-        else if (cmd == 'F') {
-            cmder = std::make_unique<FastCommand>();
-        }
-
-        if(cmder){
-            cmder->DoOperate(poseHandler);
+        const auto it = cmderMap.find(cmd);
+        if (it != cmderMap.end())
+        {
+        it->second->DoOperate(poseHandler);
         }
     }
-
-    
 }
 Pose ExecutorImpl::Query() const noexcept
 {
